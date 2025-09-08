6beb2d4838b71a842fd455c696ccf6080e7b0153
kjyothiswaroop <kjyothiswaroop00@gmail.com>
2025-09-08 11:54:56 -0500

    Added the loading functions

--- maze.py
+++ maze.py
@@ -2,10 +2,22 @@ import cell
 class Maze:
 
     def __init__(self) :
-        self.maze = [1][1]
+        self.maze = []
+        self.cell = ""
+
+    def load(self,path):
+        with open(path) as f :
+            for row_id,line in enumerate(f):
+                row = []
+                contents = line.strip().split()
+                for col_id,symbol in enumerate(contents):
+                    if(symbol == "*"):
+                        row.append(cell.Cell(1,row_id,col_id))
+                    if(symbol == "#"):
+                        row.append(cell.Cell(0,row_id,col_id))
+                self.maze.append(row)
     
-    def Load(self,path):
-        print("loading")
+                        
     
     def __str__(self):
         print("printing")
