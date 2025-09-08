1589e99be7d5b176006aa22077ecc658ab0ba311
ChenyuZhu <zhuc1915@gmail.com>
2025-09-08 10:43:16 -0500

    Finished the function for printing

--- maze.py
+++ maze.py
@@ -3,9 +3,23 @@ class Maze:
 
     def __init__(self) :
         self.maze = [1][1]
+        for row in self.maze:
+            for element in row:
+                element = Cell(True)
     
-    def Load(self,path):
+    def load(self,path):
         print("loading")
     
     def __str__(self):
         print("printing")
+        row_index = 0
+        for row in self.maze:
+            row_index += 1
+            if row_index % 2 == 1:
+                print(" ", end="")
+            for element in row:
+                if element.isFree:
+                    print(".", end=" ")
+                else:
+                    print("#", end=" ")
+                print("")
