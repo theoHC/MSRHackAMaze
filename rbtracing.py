2eb6d9ac509d2fe1db50993880cfcbae8b3c8703
theoHC <theo_coulson@me.com>
2025-09-08 16:18:54 -0500

    Recursive backtracking implemented

--- rbtracing.py
+++ rbtracing.py
@@ -1,5 +1,22 @@
 import maze
 
 class RecursiveBackTracking:
-    def __init__():
-        pass
+    def __init__(self):
+        self.path = []
+        self.visited = []
+    
+    def solve(self, problem, start, end):
+        if(start == end):
+            self.path.append(start)
+            return True
+        
+        self.visited.append(start)
+
+        for coords in maze.getAllFreeNeighbors(start) :
+            if(not coords in self.visited):
+                if(self.solve(problem, coords, end)):
+                    self.path.append(start)
+                    return True
+
+        return False
+
